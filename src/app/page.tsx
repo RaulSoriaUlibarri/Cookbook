c00578ab07bc47dc4de91e354961beaa82768a7d
Ruls <soria.developer@gmail.com>
2026-06-26 11:48:13 -0600

    chore: clean app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import { fetchCategories } from "@/server/actions";
 import MaxWidthWrapper from "../components/MaxWidthWrapper";
 
-import {
-  QueryClient,
-  HydrationBoundary,
-  dehydrate,
-} from "@tanstack/react-query";
-import NavBar from "@/components/NavBar/NavBar";
 import HeroCarousel from "@/components/UI/HeroCarousel";
 import Footer from "@/components/UI/Footer";
 
 export default async function Home() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["categories"],
-    queryFn: fetchCategories,
-  });
-
   return (
     <>
       <div className="h-[720px]">
@@ -43,18 +30,6 @@ export default async function Home() {
             </h2>
             <HeroCarousel />
           </div>
-          {/* <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -tranlate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
-            />
-          </div> */}
           <div className="mx-7 my-5 h-full md:ml-5 md:mr-0">
             <h2 className="hidden mb-5 text-2xl font-bold text-gray-900 md:block md:text-left lg:mb-20">
               Discover and Cook your Favorite Recipes
