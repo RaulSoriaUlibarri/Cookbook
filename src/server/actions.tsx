adee8c270fa76b93b0e48f87f2639c180c34e574
Ruls <soria.developer@gmail.com>
2026-06-30 19:05:25 -0600

    chore: add fetchRecipesList to actions

--- src/server/actions.tsx
+++ src/server/actions.tsx
@@ -8,6 +8,16 @@ export async function fetchCategories() {
   return data.categories;
 }
 
+export async function fetchCategoriesList() {
+  const url = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
+  const response = await fetch(url);
+
+  if (!response.ok)
+    throw new Error(`Error fetching categories, info: ${response.statusText}`);
+  const data = await response.json();
+  return data.meals;
+}
+
 export async function fetchCategoryMeals(categorySelected: String | null) {
   const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorySelected}`;
   const response = await fetch(url);
