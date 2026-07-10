ae351030b4c55a0f8468169ef451d9fccd90f73b
Ruls <soria.developer@gmail.com>
2026-07-09 20:42:53 -0600

    feat: add layout buttons to RecipesDisplay

--- src/components/RecipesDisplay.tsx
+++ src/components/RecipesDisplay.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import { useState } from "react";
 import { fetchCategoryMeals } from "@/server/actions";
 import { useQuery } from "@tanstack/react-query";
 import MealCard from "./MealCard/MealCard";
 import { RecipesPagination } from "./index";
-import { useState } from "react";
+import { LayoutList, Grid3X3 } from "lucide-react";
 
 interface RecipesDisplayProps {
   category: string | null;
 }
 
 const RecipesDisplay = ({ category }: RecipesDisplayProps) => {
+  const [listLayout, setListLayout] = useState<string>("grid");
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const recipesNum = 16;
+  const recipesNum = 18;
+
+  console.log(listLayout);
 
   const { data, error, isLoading } = useQuery({
     queryFn: () => fetchCategoryMeals(category),
@@ -24,26 +28,46 @@ const RecipesDisplay = ({ category }: RecipesDisplayProps) => {
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  // número total de páginas
   const numberOfPages = Math.ceil(data.length / recipesNum);
 
-  // calcular rango según página actual
   const start = (currentPage - 1) * recipesNum;
   const end = currentPage * recipesNum;
   const paginatedData = data.slice(start, end);
+  console.log(paginatedData);
 
   return (
     <div className="mt-16">
       <h2 className="w-full font-lexend h-auto p-5 text-4xl text-center font-bold mb-5">
         Recipe Collection
       </h2>
-      <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-3  lg:gap-8">
+      <div className="flex justify-between mb-10 max-w-15 ml-auto">
+        <button
+          className="cursor-pointer"
+          onClick={() => setListLayout("list")}
+        >
+          <LayoutList />
+        </button>
+        <button
+          className="cursor-pointer"
+          onClick={() => setListLayout("grid")}
+        >
+          <Grid3X3 />
+        </button>
+      </div>
+      <ul
+        className={
+          listLayout === "grid"
+            ? "grid grid-cols-1 gap-8 mb-10 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-3  lg:gap-8"
+            : ""
+        }
+      >
         {paginatedData.map((meal: any) => (
           <MealCard
             key={meal.idMeal}
             id={meal.idMeal}
             name={meal.strMeal}
             img={meal.strMealThumb}
+            country={meal.strCountry}
             category={category}
           />
         ))}
