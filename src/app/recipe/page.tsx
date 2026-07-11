96c1c3ebc764381b0c2a174000e02f04bbc83242
Ruls <soria.developer@gmail.com>
2026-07-10 23:22:03 -0600

    feat: include recipe page

---
+++ src/app/recipe/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import MealHero from "@/components/MealHero";
+import Footer from "@/components/UI/Footer";
+import { RecipesContainer } from "../../containers/index";
+
+export default function directory() {
+  return (
+    <>
+      <Footer />
+    </>
+  );
+}
