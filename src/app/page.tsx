e39be61a7407aa1650c5e959a4ab30ec45a2a4c8
Ruls <soria.developer@gmail.com>
2026-07-27 23:53:13 -0600

    feat: change bg color of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,19 @@ import Footer from "@/components/UI/Footer";
 
 export default async function Home() {
   return (
-    <section className="bg-[#ACE1AF]">
-      <div className="flex items-center justify-end h-[400px] bg-[url(/images/landing_page_hero.png)] bg-cover bg-center bg-no-repeat">
-        <div className="w-1/2 text-center text-black">
-          <h2 className="font-roboto-slab text-4xl md:text-6xl font-bold p-2 my-5">
-            Find Your Favorite Recipes
-          </h2>
-          <p className="text-xl md:text-2xl">
-            Discover, save, and share delicious recipes from around the world.
-          </p>
-        </div>
-      </div>
+    //bg-[#ACE1AF] provisional.
+    <section className="bg-[#f8f8f8]">
       <MaxWidthWrapper className="mb-12 mt-5 flex flex-col items-center justify-center text-center">
+        <div className="flex w-full items-center justify-end h-[400px] bg-[url(/images/landing_page_hero.png)] bg-cover bg-center bg-no-repeat">
+          <div className="w-1/2 text-center text-black">
+            <h2 className="font-roboto-slab text-4xl md:text-6xl font-bold p-2 my-5">
+              Find Your Favorite Recipes
+            </h2>
+            <p className="text-xl md:text-2xl">
+              Discover, save, and share delicious recipes from around the world.
+            </p>
+          </div>
+        </div>
         <div className="md:flex md:justify-center md:items-center lg:px-20 lg:mt-10">
           <div>
             <h2 className="my-3 px-5 font-bold text-xl text-gray-900 md:hidden lg:text-3xl">
