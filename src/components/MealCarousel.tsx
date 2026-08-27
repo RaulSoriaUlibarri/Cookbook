afdc97f4f9ada2ac49a54ca3a110fd4684b86815
Ruls <soria.developer@gmail.com>
2026-08-26 20:38:05 -0600

    chore: clean code to prevent error messages

--- src/components/MealCarousel.tsx
+++ src/components/MealCarousel.tsx
@@ -5,22 +5,14 @@ import MealCard from "./MealCard/MealCard";
 const MealCarousel = () => {
   return (
     <div>
-      <ul
-        className={
-          listLayout === "grid"
-            ? "grid grid-cols-1 gap-8 mb-10 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-3 lg:gap-8"
-            : "flex flex-col gap-4"
-        }
-      >
-        {paginatedData.map((meal: any) => (
+      <ul className="grid grid-cols-1 gap-8 mb-10 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-3 lg:gap-8">
+        {meals.map((meal: any) => (
           <MealCard
             key={meal.idMeal}
             id={meal.idMeal}
             name={meal.strMeal}
             img={meal.strMealThumb}
             country={meal.strCountry}
-            category={category}
-            variant={listLayout} // aquí decides grid o list
           />
         ))}
       </ul>
