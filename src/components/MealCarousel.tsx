882c14c2a6089a31cc28851c54ae087699867b0f
Ruls <soria.developer@gmail.com>
2026-09-07 14:56:12 -0600

    style: change mx to my of MealCarousel

--- src/components/MealCarousel.tsx
+++ src/components/MealCarousel.tsx
@@ -45,7 +45,7 @@ const MealCarousel = ({ category, title }: MealCarouselProp) => {
   };
 
   return (
-    <div className="mb-6">
+    <div className="my-6">
       <h2 className="text-left text-2xl font-bold text-gray-800 mb-4">
         {title}
       </h2>
