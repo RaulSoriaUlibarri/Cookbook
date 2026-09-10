33b4bbdb095ae9278c54e08a8fe374c846433e73
Ruls <soria.developer@gmail.com>
2026-09-09 23:59:14 -0600

    fix(HeroCarousel): Fix dark mode styles

--- src/components/UI/HeroCarousel.tsx
+++ src/components/UI/HeroCarousel.tsx
@@ -10,7 +10,7 @@ export default function HeroCarousel() {
 
   return (
     <div className="md:flex md:justify-center md:items-center lg:px-20 lg:mt-10">
-      <h2 className="my-3 px-5 font-bold text-2xl text-gray-900 md:hidden lg:text-3xl">
+      <h2 className="my-3 px-5 font-bold text-2xl text-gray-900 md:hidden lg:text-3xl dark:text-white">
         Discover and Cook your Favorite Recipes
       </h2>
       <div className="flex justify-center">
@@ -38,10 +38,10 @@ export default function HeroCarousel() {
         </div>
       </div>
       <div className="mx-7 my-5 h-full md:ml-8 md:mr-0 md:max-w-md">
-        <h2 className="hidden mb-5 text-3xl font-bold text-gray-900 md:block md:text-left lg:text-4xl lg:mb-8">
+        <h2 className="hidden mb-5 text-3xl font-bold text-gray-900 md:block md:text-left lg:text-4xl lg:mb-8 dark:text-white">
           Discover and Cook your Favorite Recipes
         </h2>
-        <p className="text-gray-700 leading-relaxed text-justify md:text-left lg:text-lg lg:leading-loose">
+        <p className="text-gray-700 leading-relaxed text-justify md:text-left lg:text-lg lg:leading-loose dark:text-white">
           Explore a wide variety of carefully selected recipes for every
           occasion. Whether you're a beginner or an experienced chef, you'll
           find inspiration for every meal. Save your favorites, share with
