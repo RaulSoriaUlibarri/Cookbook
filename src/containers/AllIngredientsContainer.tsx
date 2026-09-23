ec54cfbdef802607913941668172962cf9168c93
Ruls <soria.developer@gmail.com>
2026-09-22 23:52:45 -0600

    feature: Create AllIngredientsContainer

---
+++ src/containers/AllIngredientsContainer.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const AllIngredientsContainer = () => {
+  return (
+    <div>
+      hello
+      <span>there</span>
+    </div>
+  );
+};
+
+export default AllIngredientsContainer;
