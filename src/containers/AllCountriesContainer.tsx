9a30a343c59767053612bb706395cdea0fb67d73
Ruls <soria.developer@gmail.com>
2026-09-23 10:45:02 -0600

    style(AllCountriesContainer): dark mode

--- src/containers/AllCountriesContainer.tsx
+++ src/containers/AllCountriesContainer.tsx
@@ -44,7 +44,7 @@ const AllCountriesContainer = () => {
   return (
     <section className="pt-10">
       <MaxWidthWrapper className="px-10 md:p-none">
-        <h2 className="font-roboto-slab w-fit mx-auto text-3xl font-bold md:text-4xl md:my-10">
+        <h2 className="font-roboto-slab text-emerald-950 w-fit mx-auto text-3xl font-bold md:text-4xl md:my-10 dark:text-white">
           All Countries
         </h2>
         {alphabet.map((letter) => (
