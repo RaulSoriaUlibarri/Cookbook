5f1e6b4303aa34a7733e9b78dc989ed9aa1619e5
Ruls <soria.developer@gmail.com>
2026-09-22 20:58:52 -0600

    refactor(AlphabeticalList): Make component reusable

--- src/components/AlphabeticalList.tsx
+++ src/components/AlphabeticalList.tsx
@@ -1,17 +1,30 @@
 type AlphabeticalListProps = {
   letter: string;
   countries: string[];
+  onSelect: (country: string) => void;
 };
 
-const countriesArr = ["Algeria", "Alemania", "Argentina", "Afganistan"];
-
-const AlphabeticalList = ({ letter, countries }: AlphabeticalListProps) => {
+const AlphabeticalList = ({
+  letter,
+  countries,
+  onSelect,
+}: AlphabeticalListProps) => {
   return (
-    <div>
-      <h2>A</h2>
-      <ul className="flex ">
-        {countriesArr.map((c) => (
-          <li>{c}</li>
+    <div className="my-5">
+      <p className="font-bold text-2xl">{letter}</p>
+      <ul className="flex flex-wrap">
+        {countries.map((i) => (
+          <li
+            key={i}
+            className="text-emerald-800 hover:text-emerald-950 hover:cursor-pointer "
+          >
+            <a
+              className="block pr-5 py-3 hover:underline"
+              onClick={() => onSelect(c)}
+            >
+              {i}
+            </a>
+          </li>
         ))}
       </ul>
     </div>
