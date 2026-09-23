2b170cff030aa06d01397600fb61182f8ce884ec
Ruls <soria.developer@gmail.com>
2026-09-22 20:58:14 -0600

    feature(Containers): integrate AlphabeticalList into CountriesContainer

--- src/containers/CountriesContainer.tsx
+++ src/containers/CountriesContainer.tsx
@@ -1,13 +1,16 @@
-import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+"use client";
+
+import { AlphabeticalList } from "@/components";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { fetchCountriesList } from "@/server/actions";
 import { useQuery } from "@tanstack/react-query";
 
-const CountriesContainer = () => {
-  type Country = {
-    strArea: string;
-    strCountry: string;
-  };
+type Country = {
+  strArea: string;
+  strCountry: string;
+};
 
+const CountriesContainer = () => {
   const { data, error, isLoading } = useQuery<Country[]>({
     queryFn: fetchCountriesList,
     queryKey: ["countriesList"],
@@ -15,19 +18,44 @@ const CountriesContainer = () => {
     gcTime: 1000 * 60 * 15,
   });
 
-  if (data) {
-    for (let i = 0; i < data.length; i++) {
-      const country = data[i].strCountry;
-      countriesArray.push(country);
-    }
-  }
-
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  function searchByCountry(country: string) {
+    console.log(country);
+  }
+
+  const grouped: Record<string, string[]> = {};
+
+  if (data) {
+    for (let i = 0; i < data.length; i++) {
+      const country = data[i].strCountry;
+      const firstLetter = country[0];
+
+      if (!grouped[firstLetter]) {
+        grouped[firstLetter] = [];
+      }
+      grouped[firstLetter].push(country);
+    }
+  }
+
+  const alphabet = Object.keys(grouped).sort();
+
   return (
-    <section>
-      <MaxWidthWrapper></MaxWidthWrapper>
+    <section className="pt-10">
+      <MaxWidthWrapper className="px-10 md:p-none">
+        <h2 className="font-roboto-slab w-fit mx-auto text-3xl font-bold md:text-4xl md:my-10">
+          All Countries
+        </h2>
+        {alphabet.map((letter) => (
+          <AlphabeticalList
+            key={letter}
+            letter={letter}
+            countries={grouped[letter]}
+            onSelect={searchByCountry}
+          />
+        ))}
+      </MaxWidthWrapper>
     </section>
   );
 };
