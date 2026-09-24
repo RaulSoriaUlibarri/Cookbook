db446714686b77bbbbcc94abe7985904af9ac2e6
Ruls <soria.developer@gmail.com>
2026-09-24 15:09:46 -0600

    feature: decode ingredient of params

--- src/app/all-ingredients/[ingredient]/page.tsx
+++ src/app/all-ingredients/[ingredient]/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { use } from "react";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { decode } from "punycode";
 
-export default function IngredientRecipesPage() {
+type PageProps = {
+  params: Promise<{ ingredient: string }>;
+};
+
+export default function IngredientRecipesPage({ params }: PageProps) {
+  const { ingredient } = use(params);
+  const decodedIngredient = decodeURIComponent(ingredient);
+
   return (
     <>
-      <MaxWidthWrapper>Welcome to the page of ingredient</MaxWidthWrapper>
+      <MaxWidthWrapper>
+        Welcome to the page of {decodedIngredient}
+      </MaxWidthWrapper>
     </>
   );
 }
