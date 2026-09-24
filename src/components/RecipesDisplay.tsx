c2e8ae0f18d7297663e5a08999596c75796efc40
Ruls <soria.developer@gmail.com>
2026-09-23 18:31:03 -0600

    refactor(RecipesDisplay): remove category prop and add optional title prop

--- src/components/RecipesDisplay.tsx
+++ src/components/RecipesDisplay.tsx
@@ -1,43 +1,42 @@
+// RecipesDisplay.tsx
 "use client";
 
 import { useState } from "react";
-import { fetchCategoryMeals } from "@/server/actions";
-import { useQuery } from "@tanstack/react-query";
 import MealCard from "./MealCard/MealCard";
 import { RecipesPagination } from "./index";
 import { LayoutList, Grid3X3 } from "lucide-react";
 
+type Meal = {
+  strMeal: string;
+  strMealThumb: string;
+  idMeal: string;
+  strArea: string | null;
+  strCountry: string;
+};
+
 interface RecipesDisplayProps {
-  category: string | null;
+  meals: Meal[];
+  title?: string;
 }
 
-const RecipesDisplay = ({ category }: RecipesDisplayProps) => {
-  const [listLayout, setListLayout] = useState<string>("grid");
+const RECIPES_PER_PAGE = 18;
+
+const RecipesDisplay = ({
+  meals,
+  title = "Recipe Collection",
+}: RecipesDisplayProps) => {
+  const [listLayout, setListLayout] = useState<"grid" | "list">("grid");
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const recipesNum = 18;
 
-  console.log(listLayout);
-
-  const { data, error, isLoading } = useQuery({
-    queryFn: () => fetchCategoryMeals(category),
-    queryKey: ["meals", category],
-    enabled: !!category,
-    staleTime: 1000 * 60 * 30,
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  const numberOfPages = Math.ceil(data.length / recipesNum);
-
-  const start = (currentPage - 1) * recipesNum;
-  const end = currentPage * recipesNum;
-  const paginatedData = data.slice(start, end);
+  const numberOfPages = Math.ceil(meals.length / RECIPES_PER_PAGE);
+  const start = (currentPage - 1) * RECIPES_PER_PAGE;
+  const end = currentPage * RECIPES_PER_PAGE;
+  const paginatedData = meals.slice(start, end);
 
   return (
     <div className="mt-16">
       <h2 className="w-full font-lexend h-auto p-5 text-4xl text-center font-bold mb-5">
-        Recipe Collection
+        {title}
       </h2>
       <div className="flex justify-between mb-10 max-w-15 ml-auto">
         <button
@@ -60,14 +59,13 @@ const RecipesDisplay = ({ category }: RecipesDisplayProps) => {
             : "flex flex-col gap-4"
         }
       >
-        {paginatedData.map((meal: any) => (
+        {paginatedData.map((meal) => (
           <MealCard
             key={meal.idMeal}
             id={meal.idMeal}
             name={meal.strMeal}
             img={meal.strMealThumb}
             country={meal.strCountry}
-            category={category}
             variant={listLayout}
           />
         ))}
