df70b4bf033d1ecafcff5410be4f1857785b21af
Ruls <soria.developer@gmail.com>
2026-09-24 15:09:04 -0600

    feature: create IngredientRecipesPage

---
+++ src/app/all-ingredients/[ingredient]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function IngredientRecipesPage() {
+  return (
+    <>
+      <MaxWidthWrapper>Welcome to the page of ingredient</MaxWidthWrapper>
+    </>
+  );
+}
